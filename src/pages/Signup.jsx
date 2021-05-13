7fce16b4f14806e6cc0b134c93dd6038934d3299
puthpiseth <puthpiseth_tun@yahoo.com>
2021-05-13 20:03:17 +0200

    Fix title margin and add redirect route to login page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../styles/Signup.css';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 function Signup() {
 
+    const [username, setUsername] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [redirect, setRedirect] = useState(false);
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        axios.post('http://localhost:3000/signup', {
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            })
+        });
+        setRedirect(true);
+    }
+
+    // Redirect to login page after registered
+    if (redirect) {
+        return <Redirect to ="/login"/>;
+    }
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
             <h2>Sign up</h2>
             <div className="form-inputs">
                 <label htmlFor="username">Username</label>
@@ -13,6 +39,7 @@ function Signup() {
                 name="username"
                 className="form-input"
                 placeholder="Enter your username"
+                onChange={e => setUsername(e.target.value)}
                 />
             </div>
             <div className="form-inputs">
@@ -22,6 +49,7 @@ function Signup() {
                 name="email"
                 className="form-input"
                 placeholder="Enter your email"
+                onChange={e => setEmail(e.target.value)}
                 />
             </div>
             <div className="form-inputs">
@@ -32,6 +60,7 @@ function Signup() {
                 className="form-input"
                 placeholder="Enter your password"
                 required
+                onChange={e => setPassword(e.target.value)}
                 />
             </div>
             <button type="submit" >Signup</button>     
