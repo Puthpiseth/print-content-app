d7e0e196d5b7e64d18af15b4174c10cfd30832ce
puthpiseth <puthpiseth_tun@yahoo.com>
2021-05-13 20:12:26 +0200

    Style login page, add users in localstorage and fix route

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/Login.css';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+
 
 function Login() {
 
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const [redirect, setRedirect] = useState(false);
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        axios.post('http://localhost:3000/signup', {
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                username,
+                password
+            })
+        });
+
+        setRedirect(true);
+    }
+
+    // Redirect to login page after registered
+    if (redirect) {
+        return <Redirect to ="/"/>;
+    }
+
     return (
-        <form className="form">
+
+        <form className="form" onSubmit={handleSubmit}>
             <h2>Login</h2>
             <div className="form-inputs">
                 <label htmlFor="username">Username</label>
@@ -13,6 +40,7 @@ function Login() {
                 name="username"
                 className="form-input"
                 placeholder="Enter your username"
+                onChange={e => setUsername(e.target.value)}
                 />
             </div>
             <div className="form-inputs">
@@ -22,12 +50,12 @@ function Login() {
                 name="password"
                 className="form-input"
                 placeholder="Enter your password"
+                onChange={e => setPassword(e.target.value)}
                 />
             </div> 
             <button type="submit">Login</button>                
         </form>
     )
-    
 }
 
 export default Login;
